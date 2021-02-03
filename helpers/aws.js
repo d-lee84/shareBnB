93e96131b98e563ae144e1cb18b2c9b47e59b8a7
David Lee <davidlee980804@gmail.com>
2021-02-03 15:16:57 -0800

    Uploads to AWS now

--- helpers/aws.js
+++ helpers/aws.js
@@ -1,8 +1,8 @@
 'use strict';
 
-import { S3Client } from "@aws-sdk/client-s3";
-import {v4 as uuid} from "uuid";
-import {ACCESS_KEY,AWS_SECRET_KEY, S3_BUCKET_NAME} from "../config";
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+const { v4: uuid } = require("uuid");
+const { S3_BUCKET_NAME } = require("../config");
 
 /* S3 API
 To send a request, you:
@@ -12,21 +12,30 @@ Initiate command with input parameters.
 Call send operation on client with command object as input.
 If you are using a custom http handler, you may call destroy() to close open connections. */
 
-const config = {
-  accessKeyId: ACCESS_KEY,
-  secretAccessKey: AWS_SECRET_KEY,
-  region: "us-west-1"
-}
-const s3 = new S3Client({config});
+
+const REGION = "us-west-1"; //e.g. "us-east-1"
+// Create S3 service object
+const s3 = new S3Client({ region: REGION });
 
 // bucket: arn:aws:s3:::sharebnb-photos-dlee
-function uploadToS3Bucket(file) {
-  s3.putObject({
-    Bucket: S3_BUCKET_NAME,
-    Key: uuid(),
-    Body: file
-    // ContentType: contentType
-  });
-
-}
-module.exports = {uploadToS3Bucket};
+async function uploadToS3Bucket(file) {
+  try {
+    const data = await s3.send(new PutObjectCommand({
+      Bucket: S3_BUCKET_NAME,
+      Key: uuid(),
+      Body: file.buffer
+    }));
+    console.log("Success", data);
+  } catch (err) {
+    console.log("Error", err);
+  }
+  // console.log(s3);
+  // let resp = s3.putObject(, function(err,data) {
+  //   if(err) {
+  //    console.log(err,err.stack);
+  //   }
+  //   else {
+  //    console.log(data);
+  //   }
+};
+module.exports = { uploadToS3Bucket };
